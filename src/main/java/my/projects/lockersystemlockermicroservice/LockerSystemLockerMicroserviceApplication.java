26398daff856096cc7fd55dbc3429e755420e811
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-02 11:13:01 +0300

    adding main class

---
+++ src/main/java/my/projects/lockersystemlockermicroservice/LockerSystemLockerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package my.projects.lockersystemlockermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerSystemLockerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockerSystemLockerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/projects/lockersystemlockermicroservice/LockerSystemLockerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.lockersystemlockermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerSystemLockerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
